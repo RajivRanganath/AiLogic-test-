3871e968b55aea2c8fafbed52180dd81fc13c8f5
RAJIV RANGANATH <rajivranganath3@gmail.com>
2026-08-04 12:36:55 +0530

    Implement linked list for number addition
    Refactor code to use linked lists for input numbers and calculate their sum.

--- Problem3.py
+++ Problem3.py
@@ -1,16 +1,45 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
 n = int(input())
 list1 = list(map(int, input().split()))
 m = int(input())
 list2 = list(map(int, input().split()))
 
-result = []
-carry = 0
-for i in range(n):
-    total = list1[i] + list2[i] + carry
-    result.append(total % 10)
-    carry = total // 10
+head1 = Node(list1[0])
+curr = head1
+for i in range(1, len(list1)):
+    curr.next = Node(list1[i])
+    curr = curr.next
 
-if carry:
-    result.append(carry)
+head2 = Node(list2[0])
+curr = head2
+for i in range(1, len(list2)):
+    curr.next = Node(list2[i])
+    curr = curr.next
 
-print(' '.join(map(str, result)))
+num1 = 0
+curr = head1
+i = 0
+while curr:
+    num1 += curr.val * (10 ** i)
+    curr = curr.next
+    i += 1
+
+num2 = 0
+curr = head2
+i = 0
+while curr:
+    num2 += curr.val * (10 ** i)
+    curr = curr.next
+    i += 1
+
+result = num1 + num2
+
+result_str = str(result)
+result_digits = [int(d) for d in result_str]
+result_digits.reverse()
+
+print(' '.join(map(str, result_digits)))
